6fe899999bd3220fb200c070eea7059241bb4793
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-11-04 12:09:12 +0900

    add new image and button size changed

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
     (function(){
       'use strict';
-      const char = 3;
+      const char = 4;
       const start = document.getElementById('start');
       const next = document.getElementById('next');
       const question = document.getElementById('question');
       const drawing = document.getElementById('drawing');
       const image = document.getElementById('img_box');
-      const finish = document.getElementById('finish');
+      const reset = document.getElementById('reset');
 
       start.addEventListener('click',function(){
         start.className = 'button start hidden';
         next.className = 'button next';
         question.className = 'question hidden';
         drawing.className = '';
-        finish.className = 'button finish';
+        reset.className = 'button reset';
         next.click();
         // const randNum = Math.floor(Math.random()*char);
         // drawing.src = 'img/' + randNum + '.png';
@@ -28,12 +28,12 @@
         start.click();
       });
 
-      finish.addEventListener('click',function(){
+      reset.addEventListener('click',function(){
         start.className = 'button start';
         next.className = 'button next hidden';
         question.className = 'question';
         drawing.className = 'hidden';
-        finish.className = 'button finish hidden'
+        reset.className = 'button reset hidden'
       });
 
     })();
