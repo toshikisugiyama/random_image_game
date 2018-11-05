e4d347f180228f9f074d925cf1961dfd8c38af6b
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2018-11-05 18:40:48 +0900

    5.png 追加

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     (function(){
       'use strict';
-      const char = 5;
+      const char = 6;
       const start = document.getElementById('start');
       const next = document.getElementById('next');
       const question = document.getElementById('question');
