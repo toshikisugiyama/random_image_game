779482b6bbb986fdbb838c728ec28d0a577529f1
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-10-20 10:50:47 +0900

    implement counter

--- src/components/container/main/Counter.js
+++ src/components/container/main/Counter.js
@@ -1,7 +1,14 @@
 import React from 'react'
-const Counter = () => {
-  return(
-    <span id="counter" className="counter hidden"></span>
-  )
+const Counter = props => {
+  const number = 20
+  const counter = number - props.count + 1
+  if(props.count>0){
+    return(
+      <span className="counter">
+        {counter}
+      </span>
+    )
+  }
+  return <div></div>
 }
 export default Counter
